fc8b30d637dd8cc4caa541d95bd43b2e3e20e707
Z1yoda <ziyoda01092007@gmail.comgit init git config  --global user.email ziyoda01092007@gmil.com>
2024-03-19 23:55:59 +0500

    second

--- src/App.jsx
+++ src/App.jsx
@@ -1,72 +1,80 @@
-import './App.css'
-import barchart from './assets/barchart.svg'
-import whitecheck from './assets/whitecheck.svg'
-import login from './assets/login.svg'
-import setting from './assets/setting.svg'
-import crown from './assets/crown.svg'
-import threedots from './assets/threedots.svg'
-import plus from './assets/plus.svg'
-import caret from './assets/caret.svg'
-import { useState, useEffect } from 'react'
+import './App.css';
+import barchart from './assets/barchart.svg';
+import whitecheck from './assets/whitecheck.svg';
+import login from './assets/login.svg';
+import setting from './assets/setting.svg';
+import crown from './assets/crown.svg';
+import threedots from './assets/threedots.svg';
+import plus from './assets/plus.svg';
+import caret from './assets/caret.svg';
+import { useState, useEffect } from 'react';
 
 function App() {
-  const [minut, setMinut] = useState(0)
-  const [second, setSecond] = useState(0)
-  const [isWork, setIsWork] = useState(false)
-  const [count, setCount] = useState(0)
-  const [isActive, setIsActive] = useState(false)
-
+  const [minute, setMinute] = useState(25);
+  const [second, setSecond] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const [isBreak, setIsBreak] = useState(false);
+  const [count, setCount] = useState(0);
   const [workMinut, setWorkMinut] = useState(1)
 
   useEffect(() => {
-    setMinut(workMinut)
-    setSecond(0)
-  }, [])
 
-  useEffect(() => {
-    let interval
+    if (localStorage.getItem('count')) {
+      const savedCount = parseInt(localStorage.getItem('count'));
+      setCount(savedCount);
+    }
+
+    let interval;
     if (isActive) {
       interval = setInterval(() => {
-        if (second == 0) {
-          setMinut(minut - 1)
-          setSecond(59)
+        if (second === 0) {
+          if (minute === 0) {
+            clearInterval(interval);
+            handleComplete();
+          } else {
+            setMinute(minute - 1);
+            setSecond(59);
+          }
         } else {
-          setSecond(second - 1)
+          setSecond(second - 1);
         }
-      }, 1000)
+
+      }, 1000);
+
     }
 
+    if (minute == 0 && second == 0) {
+      setCount(count + 1);
+      localStorage.setItem('count', count);
+    }
 
+    return () => clearInterval(interval);
+  }, [isActive, minute, second]);
 
-    return () => clearInterval(interval)
-  }, [minut, second, isActive, isWork])
-
-  function timeFormat() {
-    let str = ''
-    if (minut < 10) {
-      str = '0' + minut + ":";
+  useEffect(() => {
+    if (isBreak) {
+      setMinute(5);
     } else {
-      str = minut + ":"
+      setMinute(workMinut);
     }
-
-    if (second < 10) {
-      str += '0' + second
-    } else {
-      str += second
-    }
-
-    return str
-  }
+    setSecond(0);
+  }, [isBreak]);
 
   function handleStart() {
-    setIsActive(true)
+    setIsActive(!isActive);
+  };
 
-    if (isActive) {
-      setIsActive(false)
-    }
-  }
+  function handleComplete() {
+    setIsActive(false);
+    setIsBreak(!isBreak);
+  };
 
-
+  function handleReset() {
+    setIsActive(false);
+    setIsBreak(false);
+    setMinute(workMinut);
+    setSecond(0);
+  };
 
   return (
     <>
@@ -76,30 +84,30 @@ function App() {
           Pomofocus
         </div>
         <nav>
-          <a href=""><li> <img src={barchart} alt="" /><p>Report</p></li></a>
-          <a href=""><li> <img src={setting} alt="" /><p>Setting</p></li></a>
-          <a href=""><li> <img src={login} alt="" /><p>Login</p></li></a>
-          <a href=""><li> <img src={crown} alt="" /></li></a>
-        </nav> </header>
+          <a href="/"><li> <img src={barchart} alt="" /><p>Report</p></li></a>
+          <a href="/"><li> <img src={setting} alt="" /><p>Setting</p></li></a>
+          <a href="/"><li> <img src={login} alt="" /><p>Login</p></li></a>
+          <a href="/"><li> <img src={crown} alt="" /></li></a>
+        </nav>
+      </header>
       <div className="container">
         <div className="main-wrapper">
           <div className="line"></div>
           <div className="card-wrapper">
             <div className="buttons">
-              <button>Pomodoro</button>
-              <button>Short Break</button>
-              <button>Long Break</button>
+              <button style={!isBreak ? { backgroundColor: "#a44e4e", fontWeight: "700" } : {}} onClick={() => setIsBreak(false)}>Pomodoro</button>
+              <button style={isBreak ? { backgroundColor: "#a44e4e", fontWeight: "700" } : {}} onClick={() => setIsBreak(true)}>Short Break</button>
             </div>
             <div className="time">
-              <h1>{timeFormat()}</h1>
+              <h1>{minute < 10 ? `0${minute}` : minute}:{second < 10 ? `0${second}` : second}</h1>
             </div>
             <div className="btn-wrapper">
               <button className='startBtn' style={isActive ? { backgroundColor: "white", color: "#a44e4e" } : { backgroundColor: "#a44e4e", color: "white" }} onClick={handleStart}>{isActive ? "PAUSE" : "START"}</button>
-              <span style={isActive ? { opacity: "1" } : { opacity: "0" }}><img src={caret} alt="" /></span>
+              <img onClick={handleReset} src={caret} alt="" />
             </div>
           </div>
           <div className="count-amount">
-            <p>#1</p>
+            <p>#{count}</p>
             <h3>Time to focus!</h3>
           </div>
           <div className="card-wrapper tasks">
@@ -114,9 +122,8 @@ function App() {
           </div>
         </div>
       </div>
-
     </>
-  )
+  );
 }
 
-export default App
+export default App;
